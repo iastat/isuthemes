a217fe32c43f92feee69fe1561fbb7dcb01f0672
sctyner <sctyner@iastate.edu>
2018-06-15 14:20:15 -0500

    add geom_corgi for one of the REU students :smiley: :dog:

--- R/geom.R
+++ R/geom.R
@@ -2,17 +2,18 @@
 #'
 #' @author Sam Tyner
 #' @export geom_csafe
+#' @export geom_corgi
 #'
-#' @export
+#' @examples
+#' ggplot(data = mtcars) +
+#'     geom_corgi(aes(x = drat, y = qsec))
 #'
 #'
-csafeGrob <- function(x, y, size, logo = system.file("extdata", "csafeprint.png",
-                                                     package = "csafethemes")) {
-
+csafeGrob <- function(x, y, size) {
 
   grid::rasterGrob(x             = x,
                    y             = y,
-                   image         = img,
+                   image         = csafeimg,
                    default.units = "native",
                    height        = size,
                    width         = size*0.5)
@@ -64,3 +65,59 @@ geom_csafe <- function(mapping = NULL, data = NULL, stat = "identity",
   )
 }
 
+# corgi geom for an interested REU student.
+corgiGrob <- function(x, y, size) {
+
+  grid::rasterGrob(x             = x,
+                   y             = y,
+                   image         = corgiimg,
+                   default.units = "native",
+                   height        = size,
+                   width         = size)
+}
+
+
+GeomCorgi <- ggplot2::ggproto("GeomCorgi", ggplot2::Geom,
+
+                              #draw_panel = function(., data, scales, coordinates, ...) {
+                              draw_panel = function(data, panel_scales, coord, na.rm = FALSE) {
+                                coords = coord$transform(data, panel_scales)
+                                ggplot2:::ggname("geom_corgi",
+                                                 corgiGrob(coords$x, coords$y, coords$size)
+                                )
+                              },
+
+                              non_missing_aes = c("size", "theme"),
+                              required_aes = c("x", "y"),
+                              default_aes = ggplot2::aes(size=.1),
+
+                              icon = function(.){}, # a grob representing the geom for the webpage
+
+                              desc_params = list( # description of the (optional) parameters of draw
+                              ),
+
+                              seealso = list(
+                                geom_point = ggplot2::GeomPoint$desc
+                              ),
+
+                              examples = function(.) {
+                              }
+)
+
+geom_corgi <- function(mapping = NULL, data = NULL, stat = "identity",
+                       position = "identity", na.rm = FALSE,
+                       show.legend = NA, inherit.aes = TRUE, ...) {
+  ggplot2::layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = GeomCorgi,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      na.rm = na.rm,
+      ...
+    )
+  )
+}
